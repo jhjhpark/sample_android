0a89b95f0005ad71d7ed589e67ff5a8ae3e7aaa3
jh park <navith@naver.com>
2015-01-18 11:36:56 +0900

    project created

--- ListView01/app/src/main/java/com/example/c/listview01/MainActivity.java
+++ ListView01/app/src/main/java/com/example/c/listview01/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -26,6 +27,7 @@ public class MainActivity extends ActionBarActivity {
 
 
     ArrayList<MyData> list;
+    MyCustomAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,12 +41,26 @@ public class MainActivity extends ActionBarActivity {
         list.add(new MyData(R.drawable.ic_launcher, "Title5", "SubTitle5"));
         list.add(new MyData(R.drawable.ic_launcher, "Title6", "SubTitle6"));
         list.add(new MyData(R.drawable.ic_launcher, "Title7", "SubTitle7"));
+        list.add(new MyData(R.drawable.ic_launcher, "Title1", "SubTitle1"));
+        list.add(new MyData(R.drawable.ic_launcher, "Title2", "SubTitle2"));
+        list.add(new MyData(R.drawable.ic_launcher, "Title3", "SubTitle3"));
+        list.add(new MyData(R.drawable.ic_launcher, "Title4", "SubTitle4"));
+        list.add(new MyData(R.drawable.ic_launcher, "Title5", "SubTitle5"));
+        list.add(new MyData(R.drawable.ic_launcher, "Title6", "SubTitle6"));
+        list.add(new MyData(R.drawable.ic_launcher, "Title7", "SubTitle7"));
+        list.add(new MyData(R.drawable.ic_launcher, "Title1", "SubTitle1"));
+        list.add(new MyData(R.drawable.ic_launcher, "Title2", "SubTitle2"));
+        list.add(new MyData(R.drawable.ic_launcher, "Title3", "SubTitle3"));
+        list.add(new MyData(R.drawable.ic_launcher, "Title4", "SubTitle4"));
+        list.add(new MyData(R.drawable.ic_launcher, "Title5", "SubTitle5"));
+        list.add(new MyData(R.drawable.ic_launcher, "Title6", "SubTitle6"));
+        list.add(new MyData(R.drawable.ic_launcher, "Title7", "SubTitle7"));
 
 
         ListView listView = (ListView)findViewById(R.id.listView);
 
 
-        MyCustomAdapter adapter;
+
         adapter = new MyCustomAdapter(this, R.layout.list_row, list);
         listView.setAdapter(adapter);
 
@@ -53,6 +69,27 @@ public class MainActivity extends ActionBarActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 //((TextView)view).getText().toString()
                 Toast.makeText(MainActivity.this, list.get(position).mTitle, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                int count =totalItemCount - visibleItemCount;
+                if(firstVisibleItem >=count){
+                    list.add(new MyData(R.drawable.ic_launcher, "111Title3", "SubTitle3"));
+                    list.add(new MyData(R.drawable.ic_launcher, "222Title4", "SubTitle4"));
+                    list.add(new MyData(R.drawable.ic_launcher, "333Title5", "SubTitle5"));
+                    list.add(new MyData(R.drawable.ic_launcher, "444Title6", "SubTitle6"));
+                    list.add(new MyData(R.drawable.ic_launcher, "55Title7", "SubTitle7"));
+
+                    adapter.notifyDataSetChanged();
+                }
             }
         });
 

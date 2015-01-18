7ea77bcdf1ccfb073af689c602696000b87ee230
jh park <navith@naver.com>
2015-01-18 13:21:51 +0900

    thread - view/runnable

--- Thread/app/src/main/java/com/example/c/thread/MainActivity.java
+++ Thread/app/src/main/java/com/example/c/thread/MainActivity.java
@@ -46,10 +46,10 @@ public class MainActivity extends ActionBarActivity {
             public void run() {
                 for(int i=0; i<20; i++){
                     count =i;
-                    handler.post(new Runnable() {
+                    textView.post(new Runnable() {
                         @Override
                         public void run() {
-                            textView.setText("runnable count = "+count);
+                            textView.setText("view post runnable count="+count);
                         }
                     });
 

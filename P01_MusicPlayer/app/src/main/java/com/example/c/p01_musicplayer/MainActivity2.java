1972b399dc9e87cb34a6b56b7ce123182723650b
jh park <navith@naver.com>
2015-02-01 17:35:37 +0900

    project created2

--- P01_MusicPlayer/app/src/main/java/com/example/c/p01_musicplayer/MainActivity2.java
+++ P01_MusicPlayer/app/src/main/java/com/example/c/p01_musicplayer/MainActivity2.java
@@ -1,6 +1,11 @@
 package com.example.c.p01_musicplayer;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
@@ -9,24 +14,54 @@ import android.widget.Button;
  * Created by c on 2015-02-01.
  */
 public class MainActivity2 extends ActionBarActivity {
+    MusicPlayerSerice2 mPlayerSerice2;
+    boolean mBound = false;
+
+    ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            MusicPlayerSerice2.LocalBinder2 binder2 = (MusicPlayerSerice2.LocalBinder2)service;
+            mPlayerSerice2 = binder2.getService();
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mBound = false;
+        }
+    };
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Intent intent  = new Intent(MainActivity2.this, MusicPlayerSerice2.class);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+
         Button btnPlay = (Button)findViewById(R.id.btnPlay);
         btnPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                if(mBound)
+                    mPlayerSerice2.play();
             }
         });
         Button btnStop = (Button)findViewById(R.id.btnStop);
         btnStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                if(mBound)
+                    mPlayerSerice2.stop();
             }
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mBound){
+            unbindService(mConnection);
+            mBound = false;
+        }
     }
 }
--- P01_MusicPlayer/app/src/main/java/com/example/c/p01_musicplayer/MusicPlayerSerice2.java
+++ P01_MusicPlayer/app/src/main/java/com/example/c/p01_musicplayer/MusicPlayerSerice2.java
@@ -2,13 +2,16 @@ package com.example.c.p01_musicplayer;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Binder;
+import android.os.Environment;
 import android.os.IBinder;
 
 /**
  * Created by c on 2015-02-01.
  */
 public class MusicPlayerSerice2 extends Service {
+
     public class LocalBinder2 extends Binder{
         public MusicPlayerSerice2 getService(){
             return MusicPlayerSerice2.this;
@@ -16,9 +19,33 @@ public class MusicPlayerSerice2 extends Service {
     }
 
     private LocalBinder2 mLocalBinder2 = new LocalBinder2();
+    private MediaPlayer mPlayer = null;
 
     @Override
     public IBinder onBind(Intent intent) {
         return mLocalBinder2;
+    }
+
+    public void play(){
+        String path = Environment.getExternalStorageDirectory().toString();
+        path += "/Samsung/Music/Over_the_horizon.mp3";
+
+        mPlayer = new MediaPlayer();
+        try {
+            mPlayer.setDataSource(path);
+            mPlayer.prepare();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        mPlayer.start();
+
+    }
+
+    public void stop(){
+        if(mPlayer != null){
+            mPlayer.stop();
+            mPlayer.release();
+            mPlayer = null;
+        }
     }
 }
